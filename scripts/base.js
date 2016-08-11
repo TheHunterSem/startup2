a64fb0e034d8e1f9ed10378c979ed0e7cbdcc43e
Vasyl <kiriljuk.vasja@gmail.com>
2016-08-11 19:23:05 +0300

    video

--- scripts/base.js
+++ scripts/base.js
@@ -11,13 +11,45 @@ $('.main-screen').slick({
 	arrows: false
 });
 
+
+// --------------LOGIN-POPUP--------------
+
 $(document).ready(function() { 
 	$('a#login').click( function(event){ 
 		event.preventDefault();
 		 	$('#form-login').fadeIn(500);	
 		});
-	$('a#close-popup-login').click( function(event){ 
+	
+	$('a#registration').click( function(event){ 
+		event.preventDefault();
+		 	$('#form-registration').fadeIn(500);	
+		});
+
+	$('a#close-popup').click( function(event){ 
 		event.preventDefault();
 		 	$('#form-login').fadeOut(500);	
+		 	$('#form-registration').fadeOut(500);
 		});
 	});
+
+
+$(document).ready(function() { 
+	$('a#play-btn').click( function(event){ 
+		event.preventDefault();
+		 	$('#video-wrapper-content').fadeOut(300);
+		 	$('#video').fadeIn(500);
+			 			
+		});
+	});
+
+$("a#play-btn").click(function(){
+    document.getElementById("video").play();
+    return false;
+});
+
+$("#video").click(function(){
+    document.getElementById("video").pause();
+    	$('#video').fadeOut(500);
+    	$('#video-wrapper-content').fadeIn(500);
+    return false;
+});
